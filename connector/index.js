f3fe02d1cbd010085007f39649c3fdefd867d198
Ingo Rößner <roessner@infai.org>
2017-02-28 13:27:55 +0100

    prototype

--- connector/index.js
+++ connector/index.js
@@ -14,11 +14,7 @@ _module = SeplConnector;
  Here you can register listeners see EventBus **/
 SeplConnector.prototype.init = function (config) {
     SeplConnector.super_.prototype.init.call(this, config);
-    this.controller.on(function(){
-        console.log("any event", JSON.stringify(arguments));
-    });
     this.initCom(config);
-    this.updateDeviceReg();
 };
 /** Destroy method:
  Here you have to unregister Listeners see EventBus **/
@@ -26,15 +22,23 @@ SeplConnector.prototype.stop = function () {
     SeplConnector.super_.prototype.stop.call(this);
 };
 
-SeplConnector.prototype.initCom = function(config){
-    console.log(JSON.stringify( sockets ));
+var sendCredentials = function(connection, config){
+    connection.send(JSON.stringify({user: config.user, pw: config.password}));
+};
 
-    for(var index in sockets) {
-        console.log(index);
-    }
-    var connection = new sockets.websocket('wss://echo.websocket.org');
+SeplConnector.prototype.initCom = function(config){
+    var self = this;
+    var connection = new sockets.websocket(config.sepl_url);
     connection.onopen = function () {
-        connection.send(config.user+":"+config.password);
+        console.log('WebSocket Open');
+        sendCredentials(connection, config);
+    };
+
+    connection.onclose = function(){
+        setTimeout(function () {
+            self.initCom(config);
+        },10000);
+        console.log('WebSocket Closed');
     };
 
 // Log errors
@@ -44,19 +48,34 @@ SeplConnector.prototype.initCom = function(config){
 
 // Log messages from the server
     connection.onmessage = function (e) {
-        console.log('Server: ' + e.data);
+        console.log('Server: ', e.data);
+        console.log(typeof e.data);
+        var msg = JSON.parse(e.data);
+
+        var command = msg.service_url;
+        var id = msg.device_url;
+
+        var metrics = null;
+        console.log(msg.protocol_parts); //TODO
+
+        msg.protocol_parts = self.sendCommandToZway(id, command, metrics);
+        connection.send(JSON.stringify(msg));
     };
-    /*
-    console.log("init com to sepl", JSON.stringify(config));
-    var device = this.controller.devices.get("DummyDevice_9");
-    device.performCommand("exact", {level: "32"});
-    console.log(JSON.stringify(device));
-    device.performCommand("on");
-    console.log(JSON.stringify(device));
-    */
 };
 
-SeplConnector.prototype.updateDeviceReg = function(){
-    var self = this;
-};
 
+SeplConnector.prototype.sendCommandToZway = function(id, command, metrics){
+    var device = this.controller.devices.get(id);
+    if (metrics != null){
+        device.performCommand(command);
+    }else{
+        device.performCommand(command, metrics);
+    }
+    console.log(JSON.stringify(device));
+    if(command == "update"){
+        //TODO
+        return [];
+    }else{
+        return null;
+    }
+};
