60032b9356f96c59527049dcef740ed66f529ea0
franzmueller <franzmueller@users.noreply.github.com>
2020-10-28 11:05:49 +0100

    remove unnecessary todos

--- connector-app/lib/provisioning/multi-gateway-devices/module.js
+++ connector-app/lib/provisioning/multi-gateway-devices/module.js
@@ -18,19 +18,16 @@ Modules.registerModule("provisioning/multi-gateway-devices", function (module) {
 
             result.createDevice = function(token, localId, name, deviceTypeId){
                 result.setDevice(method.set, localId, name, deviceTypeId)
-                console.log("create device called in multiGatewayMode: TODO"); // TODO
                 return {device: {name: name}}
             };
 
             result.updateDevice = function(token, remoteId, localId, name, deviceTypeId){
                 result.setDevice(method.set, localId, name, deviceTypeId)
-                console.log("update device called in multiGatewayMode: TODO"); // TODO
                 return {device: {name: name}}
             };
 
             result.deleteDevice = function(token, localId){
                 result.setDevice(method.delete, localId, name, deviceTypeId)
-                console.log("delete device called in multiGatewayMode: TODO"); // TODO
             };
 
             result.getDevice = function(token, localId){
@@ -40,17 +37,14 @@ Modules.registerModule("provisioning/multi-gateway-devices", function (module) {
 
 
             result.createHub = function(token, name, localDeviceIds, hash) {
-                console.log("create hub called in multiGatewayMode: TODO"); // TODO
                 return {hub: {hash: 0}}
             };
 
             result.updateHub = function(token, id, name, localDeviceIds, hash) {
-                console.log("update hub called in multiGatewayMode: TODO"); // TODO
                 return {hub: {hash: 0}}
             };
 
             result.getHub = function(token, id) {
-                console.log("get hub called in multiGatewayMode: TODO"); // TODO
                 return {hub: {hash: 0}}
             };
 
