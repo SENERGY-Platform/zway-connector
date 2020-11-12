84c70b3f9ff109289aa3f39545c975371d30a4a4
franzmueller <franzmueller@users.noreply.github.com>
2020-11-12 10:27:48 +0100

    remove custom timeouts from multi gateway mode

--- connector-app/lib/mqtt/module.js
+++ connector-app/lib/mqtt/module.js
@@ -99,9 +99,6 @@
             options.will_flag = true;
             options.will_topic = "device/" +  controllerId  + "/lw";
             options.will_message = "0";
-            options.ping_timeout = 1500; // Test
-            options.connect_timeout = 300;
-            options.ping_interval = 60000;
         }
 
         result.mqtt = new MQTTClient(host, port, options);
